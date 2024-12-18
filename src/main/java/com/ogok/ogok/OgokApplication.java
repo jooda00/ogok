0ef1b4b8313a9902b28621a9d66c4459705bc7b3
jooda00 <dragon2000a@gmail.com>
2024-12-18 22:25:44 +0900

    feat: first commit

---
+++ src/main/java/com/ogok/ogok/OgokApplication.java
@@ -0,0 +1,13 @@
+package com.ogok.ogok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ogok/ogok/OgokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogok.ogok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
