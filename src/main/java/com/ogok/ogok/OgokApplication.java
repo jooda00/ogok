0efcea7be019ebd9d057c9db343866c7a74cb12c
jooda00 <dragon2000a@gmail.com>
2024-12-20 16:53:52 +0900

    feat: jpa auditing 어노테이션 추가 (#5)

--- src/main/java/com/ogok/ogok/OgokApplication.java
+++ src/main/java/com/ogok/ogok/OgokApplication.java
@@ -2,8 +2,10 @@ package com.ogok.ogok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class OgokApplication {
 
 	public static void main(String[] args) {
