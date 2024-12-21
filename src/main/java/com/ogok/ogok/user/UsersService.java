f35df566c12c4c3e5b439e4826bba2c852d2f046
jooda00 <dragon2000a@gmail.com>
2024-12-21 14:50:12 +0900

    feat: 구독 취소 서비스 메소드 작성 (#7)
    - updateUser()

--- src/main/java/com/ogok/ogok/user/UsersService.java
+++ src/main/java/com/ogok/ogok/user/UsersService.java
@@ -15,7 +15,7 @@ public class UsersService {
 	private final UsersRepository usersRepository;
 
 	@Transactional
-	public void saveUsers(UsersReq usersReq) {
+	public void saveUser(UsersReq usersReq) {
 		log.info("사용자 저장 시작");
 		Users user = Users.of(usersReq);
 		if (usersRepository.findByEmail(user.getEmail()) != null) {
@@ -23,5 +23,15 @@ public class UsersService {
 		}
 		usersRepository.save(user);
 		log.info("사용자 저장 완료");
+	}
+
+	@Transactional
+	public void updateUser(Long userId) {
+		log.info("사용자 조회 시작");
+		Users user = usersRepository.findById(userId)
+			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+		log.info("사용자 조회 완료. 구독 취소 시작");
+		user.cancelSubscription();
+		log.info("구독 취소 완료");
 	}
 }
