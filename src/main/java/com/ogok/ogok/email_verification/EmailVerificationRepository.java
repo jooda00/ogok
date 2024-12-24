8a081e97872d23089d8b4b21afd943309f0e6216
jooda00 <dragon2000a@gmail.com>
2024-12-24 16:59:08 +0900

    feat: EmailVerificationRepository 생성 (#12)

---
+++ src/main/java/com/ogok/ogok/email_verification/EmailVerificationRepository.java
@@ -0,0 +1,6 @@
+package com.ogok.ogok.email_verification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+}
