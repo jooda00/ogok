3e9aa533cf43aa0379a92083c8a0c958cf242295
jooda00 <dragon2000a@gmail.com>
2024-12-24 21:12:19 +0900

    test: 인증번호 검증 및 사용자 상태 변경 테스트 (#12)

--- src/test/java/com/ogok/ogok/EmailVerification/EmailVerificationServiceTest.java
+++ src/test/java/com/ogok/ogok/EmailVerification/EmailVerificationServiceTest.java
@@ -1,5 +1,6 @@
 package com.ogok.ogok.EmailVerification;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,13 +13,19 @@ import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import com.ogok.ogok.common.EmailService;
+import com.ogok.ogok.email_verification.EmailVerification;
 import com.ogok.ogok.email_verification.EmailVerificationRepository;
 import com.ogok.ogok.email_verification.EmailVerificationService;
+import com.ogok.ogok.song.SongGenre;
+import com.ogok.ogok.user.Users;
+import com.ogok.ogok.user.UsersRepository;
+import com.ogok.ogok.user.UsersReq;
+import com.ogok.ogok.user.UsersStatus;
 
 public class EmailVerificationServiceTest {
 
 	@InjectMocks
-	private EmailVerificationService emailVerificationService; // 테스트 대상 클래스
+	private EmailVerificationService emailVerificationService;
 
 	@Mock
 	private EmailService emailService;
@@ -29,9 +36,12 @@ public class EmailVerificationServiceTest {
 	@Mock
 	private EmailVerificationRepository emailVerificationRepository;
 
+	@Mock
+	private UsersRepository usersRepository;
+
 	@BeforeEach
 	void setUp() {
-		MockitoAnnotations.openMocks(this); // Mock 초기화
+		MockitoAnnotations.openMocks(this);
 	}
 
 	@Test
@@ -53,5 +63,28 @@ public class EmailVerificationServiceTest {
 		// then
 		verify(templateEngine, times(1)).process(eq("cert"), any(Context.class)); // 템플릿 처리 확인
 		verify(emailService, times(1)).sendMail(email, "오곡 인증 메일", mockTemplateResult); // 이메일 전송 확인
+	}
+
+	@Test
+	@DisplayName("인증번호 검증 및 사용자 상태 변경 테스트")
+	void verifyVerificationCodeTest() {
+		// given
+		String email = "test@test.com";
+		String verificationCode = "123456";
+
+		Users mockUser = Users.of(new UsersReq(email, SongGenre.UPBEAT));
+		EmailVerification mockVerification = EmailVerification.of(verificationCode,	email);
+
+		when(emailVerificationRepository.findByEmail(email)).thenReturn(mockVerification);
+		when(usersRepository.findByEmail(email)).thenReturn(mockUser);
+
+		// when
+		emailVerificationService.verifyVerificationCode(email, verificationCode);
+
+		// then
+		assertTrue(mockVerification.isVerified());
+		assertEquals(UsersStatus.ACTIVE, mockUser.getStatus());
+		verify(emailVerificationRepository, times(1)).findByEmail(email);
+		verify(usersRepository, times(1)).findByEmail(email);
 	}
 }
