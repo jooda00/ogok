2a79babb59e8bccecb7d969923d107cbdac04169
jooda00 <dragon2000a@gmail.com>
2024-12-24 20:30:57 +0900

    test: sendVerificationEmail 메소드 테스트 (#12)

--- src/main/java/com/ogok/ogok/email_verification/EmailVerificationRepository.java
+++ src/main/java/com/ogok/ogok/email_verification/EmailVerificationRepository.java
@@ -3,4 +3,5 @@ package com.ogok.ogok.email_verification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+	EmailVerification findByEmail(String email);
 }
