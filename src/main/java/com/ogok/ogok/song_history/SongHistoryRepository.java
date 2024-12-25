066c3de53d01ac0cf179dc4f617ade27a0a34cc0
jooda00 <dragon2000a@gmail.com>
2024-12-25 20:36:28 +0900

    feat: SongHistoryRepository 생성 (#14)

---
+++ src/main/java/com/ogok/ogok/song_history/SongHistoryRepository.java
@@ -0,0 +1,6 @@
+package com.ogok.ogok.song_history;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SongHistoryRepository extends JpaRepository<SongHistory, Long> {
+}
