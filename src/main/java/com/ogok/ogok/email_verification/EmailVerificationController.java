5dbc33f751ef9f2c86b08c44d793eb92114cf3d9
jooda00 <dragon2000a@gmail.com>
2024-12-27 21:52:31 +0900

    feat: 구독 신청 api 생성 (#16)

---
+++ src/main/java/com/ogok/ogok/email_verification/EmailVerificationController.java
@@ -0,0 +1,38 @@
+package com.ogok.ogok.email_verification;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ogok.ogok.user.UsersReq;
+import com.ogok.ogok.user.UsersService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/subscriptions")
+@Slf4j
+public class EmailVerificationController {
+
+	private final EmailVerificationService emailVerificationService;
+	private final UsersService usersService;
+
+	@PostMapping
+	public ResponseEntity<String> subscribe(@RequestParam String email, @RequestParam String genre) {
+		try {
+			// 사용자 저장
+			usersService.saveUser(new UsersReq(email, genre));
+			// 인증 이메일 전송
+			emailVerificationService.sendVerificationEmail(email);
+			return ResponseEntity.ok("구독 신청이 완료되었습니다. 이메일을 확인해주세요.");
+		} catch (Exception e) {
+			log.error("구독 신청 처리 중 오류 발생", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("구독 신청 중 오류가 발생했습니다.");
+		}
+	}
+}
